3dc2fd0f4f7dd6456a11a0a37fbea71808e6acb5
timo-barker <73718207+timo-barker@users.noreply.github.com>
2021-04-05 19:18:27 -0700

    Update cumulative_distribution_from_rand.sql

--- cumulative_distribution_from_rand.sql
+++ cumulative_distribution_from_rand.sql
@@ -7,7 +7,7 @@ DECLARE @z_score TABLE
    ,P AS ABS(CAST(Z AS DECIMAL(1,0))-0.5*POWER(EXP(1),-1*(POWER((Z-0.5)-(1/2),2)/POWER(2*(1/POWER(PI(),2)),2))))
   );
 INSERT INTO @z_score (Z)
-VALUES (.0),(.1),(.2),(.3),(.4),(.5),(.6),(.7),(.8),(.9),(1);
+VALUES (0),(.1),(.2),(.3),(.4),(.5),(.6),(.7),(.8),(.9),(1);
 
 SELECT Z, CAST(P AS DECIMAL(5,4)) AS P
 FROM @z_score
