eaba04972a1dfe5e394aa6c4af3b827149154ad5
timo-barker <73718207+timo-barker@users.noreply.github.com>
2022-02-03 17:40:52 -0500

    Update biweekly_alphabet_assignment.sql

--- biweekly_alphabet_assignment.sql
+++ biweekly_alphabet_assignment.sql
@@ -18,21 +18,24 @@ SELECT
    ,SUBSTRING(DATENAME(WEEKDAY,dDay),1,3) AS DayDesc
    ,DATENAME(ISO_WEEK,dDay) AS ISOweek
    ,CHAR(65+((51+0.5+CAST(DATENAME(ISO_WEEK,dDay) AS INT)*0.5)%26)) AS WeekLetter
-   ,CONCAT(CASE ((DATENAME(ISO_WEEK,dDay)-1)%13)+1
+   ,CASE WHEN DATENAME(ISO_WEEK,dDay) >= 53 
+         THEN 'XX' -- Joker -- NCHAR(0x2605)
+         ELSE
+   CONCAT(CASE ((DATENAME(ISO_WEEK,dDay)-1)%13)+1
                 WHEN 1  THEN 'A' -- Ace
                 WHEN 10 THEN 'T' -- Ten
-                WHEN 11 THEN 'J' -- Jack
-                WHEN 12 THEN 'Q' -- Queen
-                WHEN 13 THEN 'K' -- King
+                WHEN 11 THEN 'J' -- Jack  -- NCHAR(0x265C)
+                WHEN 12 THEN 'Q' -- Queen -- NCHAR(0x265B)
+                WHEN 13 THEN 'K' -- King  -- NCHAR(0x265A)
                 ELSE CAST(((DATENAME(ISO_WEEK,dDay)-1)%13)+1 AS CHAR(1))
                 END
            ,CASE ((DATENAME(ISO_WEEK,dDay)-1)%4)+1
-                WHEN 1 THEN 'C' -- Club
-                WHEN 2 THEN 'D' -- Diamond
-                WHEN 3 THEN 'H' -- Heart
-                WHEN 4 THEN 'S' -- Spade
+                WHEN 1 THEN 'C' -- Clubs    -- NCHAR(0x2663)
+                WHEN 2 THEN 'D' -- Diamonds -- NCHAR(0x2666)
+                WHEN 3 THEN 'H' -- Hearts   -- NCHAR(0x2665)
+                WHEN 4 THEN 'S' -- Spades   -- NCHAR(0x2660)
                 END
-          ) AS WeeklyCard
+          ) END AS WeeklyCard
 FROM cte_dDate
 ORDER BY dDay
-OPTION (MAXRECURSION 1096);
+OPTION (MAXRECURSION 0);
